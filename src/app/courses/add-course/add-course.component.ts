251f016507e8c35bdbfd5fc0ae8973469a0c6441
Diego Imbriani <diego@greentreelabs.net>
2020-03-03 11:04:59 +0100

    add course form

--- src/app/courses/add-course/add-course.component.ts
+++ src/app/courses/add-course/add-course.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { StudioIOService, AuthenticationService } from 'src/app/_services';
 import { Router } from '@angular/router';
+import { icons } from '../../_helpers/icons';
 
 @Component({
   selector: 'app-add-course',
@@ -11,12 +12,18 @@ import { Router } from '@angular/router';
 export class AddCourseComponent implements OnInit {
 
   name;
+  icons;
+  selectedIcon;
+  colors;
 
   constructor(public toastController: ToastController,
               public router: Router,
               public io: StudioIOService,
               public auth: AuthenticationService) {
     this.name = '';
+    this.selectedIcon = '';
+    this.icons = icons;
+    this.colors = ['#f4d1d0', '#bdd2ed', '#c9ecf2', '#dce9e9'];
   }
 
   save() {
@@ -36,6 +43,10 @@ export class AddCourseComponent implements OnInit {
                 this.presentToast(e);
               });
     }
+  }
+
+  selectIcon(icon) {
+    this.selectedIcon = icon;
   }
 
   async presentToast(text) {
