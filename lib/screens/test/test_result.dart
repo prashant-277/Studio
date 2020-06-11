4a801960bba4af9d3c3c72a95e334c407ad1b86d
Diego Imbriani <diego@greentreelabs.net>
2020-06-11 18:11:05 +0200

    Test result

--- lib/screens/test/test_result.dart
+++ lib/screens/test/test_result.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:studio/constants.dart';
 import 'package:studio/courses_store.dart';
@@ -7,6 +12,8 @@ import 'package:studio/models/test_result.dart';
 import 'package:studio/screens/courses/course_screen.dart';
 import 'package:studio/screens/courses/courses_screen.dart';
 import 'package:studio/services/test_service.dart';
+import 'package:studio/widgets/circle_progress.dart';
+import 'package:studio/widgets/curved_bottom_clipper.dart';
 
 class TestResultScreen extends StatefulWidget {
   final CoursesStore store;
@@ -31,50 +38,35 @@ class _TestResultScreenState extends State<TestResultScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Container(),
         centerTitle: true,
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.white),
+        iconTheme: IconThemeData(color: kDarkBlue),
         title: Text(
-          widget.store.course.name,
-          style: TextStyle(color: Colors.white),
+          'Test result',
+          style: TextStyle(color: kDarkBlue),
         ),
-        backgroundColor: Colors.blueGrey.shade900,
-        actions: <Widget>[
-          FlatButton(
-            child: Icon(
-              LineAwesomeIcons.times_circle,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CourseScreen(widget.store)));
-            },
+        backgroundColor: Colors.blueGrey.shade50,
+        bottom: PreferredSize(
+          child: Text(
+            widget.store.course.name
           ),
-        ],
+        ),
+        leading: Container(),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Stack(
-              overflow: Overflow.visible,
-              alignment: AlignmentDirectional.center,
-              children: <Widget>[
-                Container(
-                  height: 130,
-                  color: Colors.blueGrey.shade900,
-                ),
-                Positioned(
-                  top: 34,
-                  child: circle(),
-                )
-              ],
+            ClipPath(
+              clipper: CurvedBottomClipper(),
+              child: Container(
+                color: Colors.blueGrey.shade50,
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: circle(),
+              ),
             ),
             SizedBox(
-              height: 100,
+              height: 40,
             ),
             titlePanel(),
             resultPanel(),
@@ -85,6 +77,194 @@ class _TestResultScreenState extends State<TestResultScreen> {
   }
 
   Widget circle() {
+    var value = widget.service.result().percentage();
+    return CustomPaint(
+      foregroundPainter: CircleProgress(value.toDouble()),
+      child: Container(
+        width: 200,
+        height: 200,
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: <Widget>[
+              Text(
+                "$value",
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                    color: kDarkBlue
+                ),
+              ),
+              Text(
+                "%",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: kDarkBlue
+                ),
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget noErrorsPanel() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        children: <Widget>[
+          Image(
+            image: AssetImage('assets/images/celebrate.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+            child: Text(
+              'All answers are correct, great job!',
+              style: TextStyle(fontSize: 18, color: kDarkBlue.withAlpha(200)),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget titlePanel() {
+    var text = widget.service.hasErrors() ? "Wrong answers" : "";
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget resultPanel() {
+    if (!widget.service.hasErrors()) {
+      return noErrorsPanel();
+    }
+
+    var subjects = widget.service.wrongSubjects(widget.store.subjects);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+          children: subjects
+              .map((e) => Card(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 8),
+                        child: Column(
+                          children: subjectCard(e),
+                        )),
+                  ))
+              .toList()),
+    );
+  }
+
+  subjectCard(Subject subject) {
+    List<Widget> content = [
+      Text(
+        subject.name,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+    ];
+
+    widget.service.wrongQuestionsBySubject(subject).forEach((element) {
+      content.add(ListTile(
+        title: Text(element.text),
+      ));
+    });
+
+    return content;
+  }
+}
+
+class SliverTestResultHeader implements SliverPersistentHeaderDelegate {
+  final double minExtent;
+  final double maxExtent;
+  final CoursesStore store;
+  final TestService service;
+
+  SliverTestResultHeader(
+      {this.minExtent,
+      @required this.maxExtent,
+      @required this.store,
+      @required this.service});
+
+  @override
+  Widget build(
+          BuildContext context, double shrinkOffset, bool overlapsContent) =>
+      Observer(builder: (_) {
+        return AppBar(
+          leading: Container(),
+          centerTitle: true,
+          elevation: 0,
+          primary: true,
+          iconTheme: IconThemeData(color: kContrastColor),
+          flexibleSpace: Stack(
+            fit: StackFit.loose,
+            overflow: Overflow.visible,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.black87, Colors.transparent],
+                        stops: [0, 1],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter)),
+              ),
+              Positioned(
+                top: this.maxExtent / 2 - 20,
+                right: 0,
+                left: 0,
+                child: Center(child: circle()),
+              ),
+            ],
+          ),
+          backgroundColor: kContrastDarkColor,
+          actions: <Widget>[
+            FlatButton(
+              child: Icon(
+                LineAwesomeIcons.times_circle,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CourseScreen(store)));
+              },
+            ),
+          ],
+        );
+      });
+
+  Widget circle() {
+    return CustomPaint(
+      foregroundPainter: CircleProgress(12),
+      child: Container(
+        width: 200,
+        height: 200,
+        child: Center(
+          child: Text(
+            "12 %",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+
     return Align(
       alignment: Alignment.center,
       child: Container(
@@ -117,7 +297,7 @@ class _TestResultScreenState extends State<TestResultScreen> {
               ),
             ),
             Text(
-              '${result.percentage()}%',
+              '${service.result().percentage()}%',
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 50,
@@ -133,86 +313,18 @@ class _TestResultScreenState extends State<TestResultScreen> {
     );
   }
 
-  Widget noErrorsPanel() {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 32),
-      child: Column(
-        children: <Widget>[
-          Image(
-            image: AssetImage('assets/images/celebrate.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-            child: Text(
-              'All answers are correct, great job!',
-              style: TextStyle(fontSize: 18, color: kDarkBlue.withAlpha(200)),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    );
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 
-  Widget titlePanel() {
-    var text = widget.service.hasErrors() ? "Wrong answers" : "";
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 8
-      ),
-      child: Text(text,
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
+  @override
+  FloatingHeaderSnapConfiguration get snapConfiguration => null;
 
-  Widget resultPanel() {
-    if (!widget.service.hasErrors()) {
-      return noErrorsPanel();
-    }
+  @override
+  OverScrollHeaderStretchConfiguration get stretchConfiguration => null;
 
-    var subjects = widget.service.wrongSubjects(widget.store.subjects);
-
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-          children: subjects.map((e) => Card(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 8
-                      ),
-                      child: Column(
-                        children: subjectCard(e),
-                      )
-                    ),
-                  ))
-              .toList()),
-    );
-  }
-
-  subjectCard(Subject subject) {
-    List<Widget> content = [
-      Text(subject.name,
-        style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w600
-        ),
-      ),
-    ];
-
-    widget.service.wrongQuestionsBySubject(subject).forEach((element) {
-      content.add(ListTile(
-        title: Text(element.text),
-      ));
-    });
-
-    return content;
+  sliverOpacity(double shrinkOffset) {
+    return 1.0 - max(0.0, shrinkOffset) / maxExtent;
   }
 }
